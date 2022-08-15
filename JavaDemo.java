dd6d7519acd6fedbe11c73beac16a2e001a8ae35
MargeYeh <108948141+MargeYeh@users.noreply.github.com>
2022-08-14 19:10:30 -0700

    Add files via upload

---
+++ JavaDemo.java
@@ -0,0 +1,18 @@
+package model;
+
+public class JavaDemo {
+
+	public static void main(String[] args) {
+		int Number ;
+		int myNumber = 7;
+		
+		String name ;
+		String myName = "Marge";
+		
+		System.out.println(Number);
+		System.out.println(myNumber);
+		System.out.println(name);
+		System.out.println(myName);
+	}
+
+}
